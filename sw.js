6af90e42314f252eaeae9f0c040a333d566fa47d
sighff <ssaifullah.shareef@gmail.com>
2026-03-15 13:52:53 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'a320-quiz-v2';
+const ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
